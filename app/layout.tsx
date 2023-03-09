5723286cb5bf2c8df1a3cfcd35fa355fe512d6cb
Lukasz Komar <8258799+lkomar@users.noreply.github.com>
2023-03-09 22:51:55 +0100

    Add tailwind, prettier, first styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,29 @@
-import './globals.css'
+import { Lato } from 'next/font/google';
+
+import './globals.css';
 
 export const metadata = {
   title: 'Lukasz Komar',
   description: 'App for emersoft recruitment showcase',
-}
+};
+
+const lato = Lato({
+  weight: ['300', '700'],
+  style: 'normal',
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${lato.className} bg-white text-black dark:bg-gray-900 dark:text-white`}
+    >
       <body>{children}</body>
     </html>
-  )
+  );
 }
