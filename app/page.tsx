3d6cc5cac8ad50031b8afe2acbfe037dcbd8d68a
Lukasz Komar <8258799+lkomar@users.noreply.github.com>
2023-03-10 22:11:20 +0100

    Create first basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <main>Hello world</main>;
+  return (
+    <main className="grid grid-rows-1">
+      <div className="flex flex-col items-center">
+        <h1>From the blog</h1>
+        <h4 className="max-w-xl text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+          numquam tempora magnam.
+        </h4>
+      </div>
+      <div className="flex h-96 w-full items-center justify-center">Posts</div>
+    </main>
+  );
 }
