a062564c0321bf6714bfeac9fda70fd4fc31f923
Lukasz Komar <8258799+lkomar@users.noreply.github.com>
2023-03-11 21:45:04 +0100

    Split blog into separate routes

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from 'next/server';
 import blog from '@/public/blog.json';
 
 export const GET = (request: Request) => {
-  return NextResponse.json(blog);
+  return NextResponse.json(blog.categories);
 };
---
+++ app/api/posts/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server';
+
+import blog from '@/public/blog.json';
+
+export const GET = (request: Request) => {
+  return NextResponse.json(blog.posts);
+};
